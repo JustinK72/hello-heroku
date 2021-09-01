3047a437e01ea76a69e1316305bfc6cf3634cb2f
Justin Knight <jknight4@una.edu>
2021-09-01 12:51:20 -0500

    second time comment

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('hello node');
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+res.send('Hello World')
+})
+
+app.listen(process.env.PORT || 3000)
