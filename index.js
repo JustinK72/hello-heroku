ebab62861468047664169433be7373afef876066
Justin <89216592+JustinK72@users.noreply.github.com>
2021-09-02 13:39:21 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-    res.send('<h3> have you ever seen a man say goodbye to a pair of shoes before? </h3>')
+    res.send('<h3>have you ever seen a man say goodbye to a pair of shoes before?</h3> <a href = "https://github.com/JustinK72/hello-heroku"> thrillhouse </a> ')
 });
 
 app.get('/about', function (req, res) {
